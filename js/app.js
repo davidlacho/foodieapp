4411ecb008eb656a8f06317e2bb272023513db1d
David Lacho <david.lacho@gmail.com>
2018-06-29 15:46:33 +0900

    Made a successful query to server and able to list all items

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
+// ==== GET API KEYS FROM CONFIG.JS FILE =====
+const config = require('./config.js');
 const food2forkApiKey = config.food2forkApiKey;
 const mashApeKey = config.mashApeKey;
+const fetch = require('node-fetch');
+
+// ===== FETCH HANDLERS =====
 
 function fetchData(url, params) {
   return fetch(url, params)
     .then(res => checkStatus(res))
+    .then(res => res.json())
     .catch(error => console.log('Looks like there was a problem: ', error));
 }
-
 
 function checkStatus(res) {
   if (res.ok) {
@@ -16,14 +21,26 @@ function checkStatus(res) {
   }
 }
 
+// ===== FETCH DATA FROM FOOD2FORK =====
 
-const url = `https://community-food2fork.p.mashape.com/search?key=${food2forkApiKey}&q=cream+cheese`
+let ingredient = "cream+cheese";
+
+const url = `https://community-food2fork.p.mashape.com/search?key=${food2forkApiKey}&q=${ingredient}`
 const params = {
   headers: {
-    "X-Mashape-Key": "mashApeKey",
-    "Accept": "application/json"
+    "X-Mashape-Key": mashApeKey,
   }
 }
 
-fetchData(url, params)
-.then(data => console.log(data));
+// Set to true to actually perform fetch:
+const doFetch = false;
+
+if (doFetch){
+  fetchData(url, params)
+  .then((data) => {
+    const recipes = data.recipes;
+    for (let recipe in recipes) {
+      console.log(recipes[recipe].title);
+   }
+  });
+}
