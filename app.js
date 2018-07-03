ca4faf8384b213cbc272161f7c7f7eec0e97b5ec
David Lacho <david.lacho@gmail.com>
2018-07-03 14:59:01 +0900

    R1D6. Setting up more routing & fixed API calls.

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ app.listen(3000, ()=> {
 app.set('view engine', 'pug');
 app.use(router);
 app.use(express.static('public'));
+
+// any request that makes it this far will run this function:
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+// error handling:
+app.use((err, req, res, next) =>{
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('template', {
+    message: err
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const fetch = require('node-fetch');
 // ==== GET API KEYS FROM CONFIG.JS FILE =====
 const config = require('../js/config.js');
 const food2forkApiKey = config.food2forkApiKey;
-const mashApeKey = config.mashApeKey;
 
 // ===== FETCH HANDLERS =====
 
-function fetchData(url, params) {
-  return fetch(url, params)
+function fetchData(url) {
+
+  return fetch(url)
     .then(res => checkStatus(res))
     .then(res => res.json())
     .catch(error => console.log('Looks like there was a problem: ', error));
@@ -24,28 +24,43 @@ function checkStatus(res) {
   }
 }
 
+// ===== HANDLE ROUTES =====
+
+
 router.get('/', (req, res, next) => {
+  let { ingredient } = req.query;
+  let { recipe } = req.query;
 
-  let {
-    ingredient
-  } = req.query;
-  if (ingredient != undefined) {
-    let recipes = fetch(ingredient);
-    const url = `https://community-food2fork.p.mashape.com/search?key=${food2forkApiKey}&q=${ingredient}`
-    const params = {
-      headers: {
-        "X-Mashape-Key": mashApeKey,
-      }
-    }
-
-    fetchData(url, params).
-    then((data) => {
-      res.render('template', data);
-    });
+  if (recipe != undefined) {
+    url = `http://food2fork.com/api/get?key=${food2forkApiKey}&rId=${recipe}`;
+    console.log(url);
+    fetchData(url)
+      .then((data) => {
+        recipeIngredients = data;
+        console.log(data);
+      })
+    res.render('template');
+  } else if (ingredient != undefined) {
+    let recipes = [];
+    const url = `http://food2fork.com/api/search?key=${food2forkApiKey}&q=${ingredient}`;
+    fetchData(url)
+      .then((data) => {
+        recipes = data;
+        if (recipes.recipes.length === 0) {
+          res.render('template', {
+            message : "Nothing found! Try again."
+          });
+          // NOTHING RETURNED
+        } else {
+          recipes.recipes.forEach((recipe) => {
+            recipe.social_rank = Math.round(recipe.social_rank);
+          });
+          res.render('template', recipes);
+        }
+      });
   } else {
     res.render('template');
   }
 });
-
 
 module.exports = router;
